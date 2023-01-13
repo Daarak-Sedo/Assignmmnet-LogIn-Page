5e57f623b914262658a7d9dc42f29d360137d352
Bhuwan Singh Samant <bhuwansinghsamant@Bhuwans-MacBook-Air.local>
2023-01-13 11:27:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import image from "./images/img.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="left-side-wrapper">
+          <img src={image}></img>
+        </div>
+        <div className="right-side-wrapper">
+          <form>
+            <h1 style={{ fontSize: "40px" }}>Log In</h1>
+
+            <div className="input-section1">
+              <label>LogIn ID</label>
+              <input
+                type="text"
+                id="input-box1"
+                placeholder="Enter LogIn Id"
+              ></input>
+            </div>
+            <div className="input-section2">
+              <label>Password</label>
+              <input
+                type="password"
+                id="input-box2"
+                placeholder="Enter Password"
+              ></input>
+            </div>
+
+            <div className="footer">
+              <input type="checkbox" id="input-box3"></input>
+
+              <p id="p1">Remember Me</p>
+
+              <a href="" style={{ color: " #F78719", marginLeft: "120px" }}>
+                {" "}
+                <p id="p2">Change Password</p>
+              </a>
+            </div>
+
+            <div className="footer2">
+              <input type="checkbox" id="input-box4"></input>
+              <p id="p3">
+                Agree to
+                <a href="" style={{ color: " #F78719" }}>
+                  Terms & Conditions
+                </a>{" "}
+              </p>
+            </div>
+
+            <div className="footer3">
+              <button
+                type="Submit"
+                style={{ color: "white", fontSize: "22px" }}
+              >
+                LogIn
+              </button>
+
+              <p id="p4">
+                Don't have an account?
+                <a href="" style={{ color: " #F78719" }}>
+                  Register Here
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
